8a4cac74f5c60a786781dcf3b779d7ea7e739ad5
jimmidill <jimmidill@gmail.com>
2022-04-04 16:37:30 +0100

    Add console_script and main function

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -20,3 +20,20 @@ def create_parser():
   )
 
   return parser 
+
+def main():
+  import boto3
+  from pgbackup import pgdump, storage 
+
+  args = create_parser().parse_args()
+  dump = pgdump.dump(args.url)
+
+  if args.driver == 's3':
+    client = boto3.client('s3')
+
+    # TODO: create better name based on db name and date/time
+
+    storage.s3(client, dump.stdout, args.destination, 'example.sql')
+  else:
+    outfile = open(args.destination, 'wb')
+    storage.local(dump.stdout, outfile)
